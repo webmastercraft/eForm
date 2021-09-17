1895d779b9f71aeecdb41684887f3a3ea2690d67
Iakov Agababian <iakov.agababian2020@gmail.com>
2021-09-17 23:39:32 +0300

    complete printer using fpdf

--- print1.php
+++ print1.php
@@ -1,4 +1,5 @@
 <?php
+// echo 1;exit;
 //define('FPDF_FONTPATH','/font');
 require('PDF1.php');
 require("FPDI/fpdi.php");
@@ -20,6 +21,24 @@ class PDF extends FPDF{
     
 }
 
+$post = [
+    'action' => 'load',
+    'pageName' => 'softform_1',
+];
+
+$ch = curl_init("https://jeavensoftcdn.com/icarus/V2Update/test.php");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$data = curl_exec($ch);
+$data = json_decode($data, true);
+curl_close ($ch);
+$keys = array_keys($data);
+
+function get_checked($key) {
+    global $data;
+    $result = $data[$key]=="true" ? "4" : '';
+    return $result;
+}
 
 $pdf = new PDF();
 
@@ -34,67 +53,71 @@ $pdf->Ln();
 $pdf->SetFont('Arial','B',14);
 $pdf->Cell(12, 10, 'Re:');
 $pdf->SetFont('Arial','',14);
-$pdf->Cell($width - 12, 10, 'printer test', 0, 1);
+$pdf->Cell($width - 12, 10, $data['form_1_0'], 0, 1);
 
 $pdf->SetFont('Arial','B',14);
 $pdf->Cell(65, 10, 'To Whom It May Concern:');
 $pdf->SetFont('Arial','',14);
-$pdf->Cell($width - 65, 10, 'sdfsd', 0, 1);
+$pdf->Cell($width - 65, 10, $data['form_1_1'], 0, 1);
 
-$pdf->MultiCell($width, 10, 'Surgical stockings have been prescribed for the above-mentioned patient insured by your company.', 0, 1);
+$pdf->MultiCell($width, 5, 'Surgical stockings have been prescribed for the above-mentioned patient insured by your company.', 0, 1);
 
 //1th check group
 $pdf->SetFont('Arial','B',14); $pdf->Cell($width, 10, 'Symptoms:', 0, 1);
 //show check mark
-$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, "4", 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/2, 5, "tired legs and feet");
-$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, "", 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/2, 5, "varicosities", 0, 1);
+$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, get_checked('form_1_1_check'), 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/2, 5, "tired legs and feet");
+$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, get_checked('form_1_2_check'), 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/2, 5, "varicosities", 0, 1);
+$pdf->Cell($width, 1.5, '', 0, 1);
 
-$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, "", 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/2, 5, "aching, fatigued legs");
-$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, "4", 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/2, 5, "edema", 0, 1);
+$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, get_checked('form_1_3_check'), 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/2, 5, "aching, fatigued legs");
+$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, get_checked('form_1_4_check'), 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/2, 5, "edema", 0, 1);
+$pdf->Cell($width, 1.5, '', 0, 1);
 
-$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, "4", 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/2, 5, "ankle and leg swelling");
-$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, "4", 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/2, 5, "thrombophlebitis", 0, 1);
+$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, get_checked('form_1_5_check'), 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/2, 5, "ankle and leg swelling");
+$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, get_checked('form_1_6_check'), 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/2, 5, "thrombophlebitis", 0, 1);
+$pdf->Cell($width, 1.5, '', 0, 1);
 
-$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, "4", 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/2, 5, "poor circulation");
-$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, "", 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/2, 5, "pregnancy", 0, 1);
+$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, get_checked('form_1_7_check'), 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/2, 5, "poor circulation");
+$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, get_checked('form_1_8_check'), 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/2, 5, "pregnancy", 0, 1);
+$pdf->Cell($width, 1.5, '', 0, 1);
 
-$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, "4", 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/2, 5, "Other:", 0, 1);
+$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, get_checked('form_1_9_check'), 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/2, 5, "Other:".$data['form_1_10'], 0, 1);
 
 
 $pdf->SetFont('Arial','B',14);
 $pdf->Cell(75, 10, 'Date of examination performed: ');
 $pdf->SetFont('Arial','',14);
-$pdf->Cell($width - 75, 10, 'test test ', 0, 1);
+$pdf->Cell($width - 75, 10, $data['form_1_11'], 0, 1);
 
 //2th check group
 $pdf->SetFont('Arial','B',14); $pdf->Cell($width, 10, 'Compression Stocking Manufacturer:', 0, 1);
 //show check mark
-$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, "4", 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/4, 5, "Jobst");
-$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, "", 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/4, 5, "Therafirm", 0, 1);
+$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, get_checked('form_1_12_check'), 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/4 - 5, 5, "Jobst");
+$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, get_checked('form_1_13_check'), 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/4, 5, "Therafirm", 0, 1);
 $pdf->Ln();
 
 //3th check group
 $pdf->SetFont('Arial','B',14); $pdf->Cell($width/4, 5, 'Compression: ');
 //show check mark
-$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, "", 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/4, 5, "15 - 20 mmHg");
-$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, "4", 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/4, 5, "20 - 30 mmHg");
-$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, "", 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/4, 5, "30 - 40 mmHg", 0, 1);
+$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, get_checked('form_1_14_check'), 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/4, 5, "15 - 20 mmHg");
+$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, get_checked('form_1_15_check'), 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/4, 5, "20 - 30 mmHg");
+$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, get_checked('form_1_16_check'), 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/4, 5, "30 - 40 mmHg", 0, 1);
 $pdf->Ln();
 
-//3th check group
+//4th check group
 $pdf->SetFont('Arial','B',14); $pdf->Cell($width/4, 5, 'Style: ');
 //show check mark
-$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, "", 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/6, 5, "knee");
-$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, "4", 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/6, 5, "thigh");
-$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, "4", 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/6, 5, "waist");
-$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, "", 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/6, 5, "maternity", 0, 1);
+$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, get_checked('form_1_17_check'), 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/6, 5, "knee");
+$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, get_checked('form_1_18_check'), 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/6, 5, "thigh");
+$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, get_checked('form_1_19_check'), 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/6, 5, "waist");
+$pdf->SetFont('ZapfDingbats','', 15); $pdf->Cell(5, 5, get_checked('form_1_20_check'), 1, 0, 'C'); $pdf->SetFont('Arial','',14); $pdf->Cell($width/6, 5, "maternity", 0, 1);
 $pdf->Ln();
 
 
 $pdf->SetFont('Arial','B',14);
 $pdf->Cell(40, 10, 'Comments: ');
 $pdf->SetFont('Arial','',14);
-$pdf->Cell($width - 40, 10, 'test test ', 0, 1);
+$pdf->Cell($width - 40, 10, $data['form_1_21'], 0, 1);
 
 $pdf->SetFont('Arial','',12);
 $pdf->Cell($width, 5, 'Cost of the surgical stockings is shown on the attached receipt. ', 0, 1);
@@ -102,7 +125,7 @@ $pdf->Cell($width, 5, 'Please feel free to contact me if you have any further qu
 $pdf->Cell($width, 5, 'Sincerely,', 0, 1);
 $pdf->Ln();
 
-$pdf->Cell($width, 10, 'test test ', 0, 1, 'C');
+$pdf->Cell($width, 10, $data['form_1_22'], 0, 1, 'C');
 $pdf->Cell($width, 10, 'Dr. Mark Renzoni D.C., R.M.T., H.B.Sc. ', 0, 1, 'C');
 
 $pdf->Output();
